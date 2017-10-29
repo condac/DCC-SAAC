902a57458be5521dc2aafb6ca273e71cae2776c8
condac <burns.nu@gmail.com>
2017-10-29 21:02:59 +0100

    Updated Kjell remote with right values

--- DCC_SAAC/remote_kjell.h
+++ DCC_SAAC/remote_kjell.h
@@ -1,22 +1,40 @@
 //Luxorparts IR-mottagare och fjärrkontroll för Arduino
 // https://www.kjell.com/se/sortiment/el-verktyg/arduino/moduler/luxorparts-ir-mottagare-och-fjarrkontroll-for-arduino-p87895
 
-#define REMOTE_LEFT   10
-#define REMOTE_RIGHT  30
-#define REMOTE_UP     5
-#define REMOTE_DOWN   2
+#define REMOTE_LEFT   68
+#define REMOTE_RIGHT  67
+#define REMOTE_UP     70
+#define REMOTE_DOWN   21
 #define REMOTE_SELECT 64
-#define REMOTE_EMERGENCY  77
-#define REMOTE_POWER_ON 22
-#define REMOTE_1 9
-#define REMOTE_2 29
-#define REMOTE_3 31
-#define REMOTE_4 13
-#define REMOTE_5 25
-#define REMOTE_6 27
-#define REMOTE_7 17
-#define REMOTE_8 21
-#define REMOTE_9 23
-#define REMOTE_0 18
+#define REMOTE_EMERGENCY  66
+#define REMOTE_POWER_ON 74
+#define REMOTE_1 22
+#define REMOTE_2 25
+#define REMOTE_3 13
+#define REMOTE_4 12
+#define REMOTE_5 24
+#define REMOTE_6 94
+#define REMOTE_7 8
+#define REMOTE_8 28
+#define REMOTE_9 90
+#define REMOTE_0 82
 
 #define REMOTE_HOLD_ADDRESS 65535
+
+/*IR: 65280 22 "1"
+IR: 65280 25 "2"
+IR: 65280 13 "3"
+IR: 65280 12 "4"
+IR: 65280 24 "5"
+IR: 65280 94 "6"
+IR: 65280 8  "7"
+IR: 65280 28 "8"
+IR: 65280 90 "9"
+IR: 65280 66 "*"
+IR: 65280 82 "0"
+IR: 65280 74 "#"
+IR: 65280 70 "UP"
+IR: 65280 21 "DOWN"
+IR: 65280 68 "LEFT"
+IR: 65280 67 "RIGHT"
+IR: 65280 64 "OK"*/
